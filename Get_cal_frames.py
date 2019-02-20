38fb1db9fa4577c84f486a114d5cbc88bf972f42
jfausett <jfausett@nevada.unr.edu>
2019-02-20 08:22:13 -0800

    finished with version 1

--- Get_cal_frames.py
+++ Get_cal_frames.py
@@ -1,30 +1,22 @@
 import shutil, os
 import pandas as pd
-import numpy as np
 from jdcal import jd2gcal, gcal2jd
+import calgui as cg
 
-dates = input('What dates do you need calibration frames for\n'
-              '*Must enter as a list of strings*\n'
-              'Example: ["20180901","20190105",...]\n')
+try:
+    types, outpath, binning, temp, dates, exposures, filters = cg.gui()
+except:
+    print '\nExiting based on GUI call'
+    exit()
 
-bins = input('What binning do you need\n'
-             '*Must enter as a list of strings*\n'
-             'Example: ["1X1"]\n')
+userJD = []
+for date in dates:
+    jd1, jd2 = gcal2jd(int(date[0:4]), int(date[4:6]), int(date[6:]))
+    userJD.append(jd1 + jd2)
 
-filters = input('What filters do you need\n'
-                '*Must enter as a list of strings*\n'
-                'Example: ["V","Red","Luminance",...]\n')
-
-exposures = input('What exposure times do you need\n'
-                  '*Must enter as a list of strings*\n'
-                  'Example: ["030", "300"]\n')
-
-print dates, bins, filters, exposures
-print type(dates), type(bins), type(filters), type(exposures)
 
 sjd = 2400000.5
 mstdir = 'Z:\Calibration Master Frames\\'
-outpath = 'C:\Users\user\Desktop\Test_out'
 
 if not os.path.exists(outpath):
     os.mkdir(outpath)
@@ -32,85 +24,80 @@ if not os.path.exists(outpath):
 cal_files = pd.DataFrame()
 cal_files = pd.read_pickle(mstdir+'all_files.pkl')
 
+binfiles = cal_files.where(cal_files['binning'] == binning)
+binfiles.dropna(how='all', inplace=True)
 
-for bns in bins:
-    for date in dates:
-        filter1 = cal_files['type'] == 'Bias Frame'
-        filter2 = cal_files['binning'] == bns
-        bias_files = cal_files.where(filter1 & filter2)
-        bias_files.dropna(how='all', inplace=True)
-        year, month, day = int(date[0:4]), int(date[4:6]), int(date[6:])
-        jd1, jd2 = gcal2jd(year, month, day)
-        jddate = jd1 + jd2
-        bdates = bias_files['JD'].tolist()
-        bdates = np.unique(bdates)
-        biasdate = min(bdates, key=lambda x: abs(int(x) - jddate))
+tempfiles = binfiles.where(binfiles['temp'] == temp)
+tempfiles.dropna(how='all', inplace=True)
+
+
+if 'Bias' in types:
+    print 'Copying master bias files'
+    bias_files = tempfiles.where(tempfiles['type'] == 'Bias Frame')
+    bias_files.dropna(how='all', inplace=True)
+    biasJD = bias_files['JD']
+    for date in userJD:
+        year, month, day, sec = jd2gcal(sjd, (date - sjd))
+        usertag = '{}{}{}'.format(str(year), str(month).zfill(2), str(day).zfill(2))
+        biasdate = min(biasJD, key=lambda x: abs(x - date))
         year, month, day, sec = jd2gcal(sjd, (biasdate - sjd))
-        year, month, day = str(year), str(month), str(day)
-        if len(month) == 1:
-            month = '0' + month
-        if len(day) == 1:
-            day = '0' + day
-        tagdate = year + month + day
+        tagdate = '{}{}{}'.format(str(year), str(month).zfill(2), str(day).zfill(2))
+        fname = '{}{}\\Bias\\{}\\master_bias_{}_{}.fits'.format(mstdir, binning, tagdate, tagdate, binning)
+        outname = fname.replace(mstdir + binning + '\\Bias\\' + tagdate,\
+                                outpath + usertag + '\\')
+        if not os.path.exists(outpath + usertag + '\\'):
+            os.mkdir(outpath + usertag + '\\')
         try:
-            sourcepath = '{}{}\Bias\{}\master_bias_{}_{}.fits'.format(mstdir, bns, tagdate, tagdate, bns)
-            shutil.copy(sourcepath, outpath)
+            shutil.copy(fname, outname)
         except:
-            sourcepath = '{}{}\Bias\{}\master_bias_{}_{}_1.fits'.format(mstdir, bns, tagdate, tagdate, bns)
-            shutil.copy(sourcepath, outpath)
+            shutil.copy(fname.replace('.fits', '_1.fits'), outname)
 
-for bns in bins:
+
+if 'Dark' in types:
+    print 'Copying master dark files'
+    dark_files = tempfiles.where(tempfiles['type'] == 'Dark Frame')
+    dark_files.dropna(how='all', inplace=True)
     for exp in exposures:
-        for date in dates:
-            filter1 = cal_files['type'] == 'Dark Frame'
-            filter2 = cal_files['binning'] == bns
-            filter3 = cal_files['exp'] == exp
-            dark_files = cal_files.where(filter1 & filter2 & filter3)
-            dark_files.dropna(how='all', inplace=True)
-            year, month, day = int(date[0:4]), int(date[4:6]), int(date[6:])
-            jd1, jd2 = gcal2jd(year, month, day)
-            jddate = jd1 + jd2
-            ddates = dark_files['JD'].tolist()
-            ddates = np.unique(ddates)
-            darkdate = min(ddates, key=lambda x: abs(int(x) - jddate))
+        darkexp = dark_files.where(dark_files['exp'] == exp)
+        darkexp.dropna(how='all', inplace=True)
+        darkJD = darkexp['JD']
+        for date in userJD:
+            year, month, day, sec = jd2gcal(sjd, (date - sjd))
+            usertag = '{}{}{}'.format(str(year), str(month).zfill(2), str(day).zfill(2))
+            darkdate = min(darkJD, key=lambda x: abs(x - date))
             year, month, day, sec = jd2gcal(sjd, (darkdate - sjd))
-            year, month, day = str(year), str(month), str(day)
-            if len(month) == 1:
-                month = '0' + month
-            if len(day) == 1:
-                day = '0' + day
-            tagdate = year + month + day
+            tagdate = '{}{}{}'.format(str(year), str(month).zfill(2), str(day).zfill(2))
+            fname = '{}{}\\Dark\\{}\\master_dark_{}_{}_{}.fits'.format(mstdir, binning, tagdate, tagdate, binning, exp)
+            outname = fname.replace(mstdir + binning + '\\Dark\\' + tagdate,\
+                                    outpath + usertag + '\\')
+            if not os.path.exists(outpath + usertag + '\\'):
+                os.mkdir(outpath + usertag + '\\')
             try:
-                sourcepath = '{}{}\Dark\{}\master_dark_{}_{}_{}.fits'.format(mstdir, bns, tagdate, tagdate, bns, exp)
-                shutil.copy(sourcepath, outpath)
+                shutil.copy(fname, outname)
             except:
-                sourcepath = '{}{}\Dark\{}\master_dark_{}_{}_{}_1.fits'.format(mstdir, bns, tagdate, tagdate, bns, exp)
-                shutil.copy(sourcepath, outpath)
+                shutil.copy(fname.replace('.fits', '_1.fits'), outname)
 
-for bns in bins:
+
+if 'Flat' in types:
+    print 'Copying master flat files'
+    flat_files = tempfiles.where(tempfiles['type'] == 'Flat Field')
+    flat_files.dropna(how='all', inplace=True)
     for band in filters:
-        for date in dates:
-            filter1 = cal_files['type'] == 'Flat Field'
-            filter2 = cal_files['binning'] == bns
-            filter3 = cal_files['filter'] == band
-            flat_files = cal_files.where(filter1 & filter2 & filter3)
-            flat_files.dropna(how='all', inplace=True)
-            year, month, day = int(date[0:4]), int(date[4:6]), int(date[6:])
-            jd1, jd2 = gcal2jd(year, month, day)
-            jddate = jd1 + jd2
-            fdates = flat_files['JD'].tolist()
-            fdates = np.unique(fdates)
-            flatdate = min(fdates, key=lambda x: abs(int(x) - jddate))
+        flatband = flat_files.where(flat_files['filter'] == band)
+        flatband.dropna(how='all', inplace=True)
+        flatJD = flatband['JD']
+        for date in userJD:
+            year, month, day, sec = jd2gcal(sjd, (date - sjd))
+            usertag = '{}{}{}'.format(str(year), str(month).zfill(2), str(day).zfill(2))
+            flatdate = min(flatJD, key=lambda x: abs(x - date))
             year, month, day, sec = jd2gcal(sjd, (flatdate - sjd))
-            year, month, day = str(year), str(month), str(day)
-            if len(month) == 1:
-                month = '0' + month
-            if len(day) == 1:
-                day = '0' + day
-            tagdate = year + month + day
+            tagdate = '{}{}{}'.format(str(year), str(month).zfill(2), str(day).zfill(2))
+            fname = '{}{}\\Flat\\{}\\master_flat_{}_{}_{}.fits'.format(mstdir, binning, tagdate, tagdate, binning, band)
+            outname = fname.replace(mstdir + binning + '\\Flat\\' + tagdate,\
+                                    outpath + usertag + '\\')
+            if not os.path.exists(outpath + usertag + '\\'):
+                os.mkdir(outpath + usertag + '\\')
             try:
-                sourcepath = '{}{}\Flat\{}\master_flat_{}_{}_{}.fits'.format(mstdir, bns, tagdate, tagdate, bns, band)
-                shutil.copy(sourcepath, outpath)
+                shutil.copy(fname, outname)
             except:
-                sourcepath = '{}{}\Flat\{}\master_flat_{}_{}_{}_1.fits'.format(mstdir, bns, tagdate, tagdate, bns, band)
-                shutil.copy(sourcepath, outpath)
+                shutil.copy(fname.replace('.fits', '_1.fits'), outname)

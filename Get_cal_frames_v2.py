4c77374e8b3d2bd212fe32821edc89e0f201e7ef
jfausett <jfausett@nevada.unr.edu>
2019-02-20 06:52:57 -0800

    Done with Bias, Dark. First version flat

--- Get_cal_frames_v2.py
+++ Get_cal_frames_v2.py
@@ -27,7 +27,9 @@ binfiles.dropna(how='all', inplace=True)
 tempfiles = binfiles.where(binfiles['temp'] == temp)
 tempfiles.dropna(how='all', inplace=True)
 
+
 if 'Bias' in types:
+    print 'Copying master bias files'
     bias_files = tempfiles.where(tempfiles['type'] == 'Bias Frame')
     bias_files.dropna(how='all', inplace=True)
     biasJD = bias_files['JD']
@@ -49,6 +51,7 @@ if 'Bias' in types:
 
 
 if 'Dark' in types:
+    print 'Copying master dark files'
     dark_files = tempfiles.where(tempfiles['type'] == 'Dark Frame')
     dark_files.dropna(how='all', inplace=True)
     for exp in exposures:
@@ -63,6 +66,31 @@ if 'Dark' in types:
             tagdate = '{}{}{}'.format(str(year), str(month).zfill(2), str(day).zfill(2))
             fname = '{}{}\\Dark\\{}\\master_dark_{}_{}_{}.fits'.format(mstdir, binning, tagdate, tagdate, binning, exp)
             outname = fname.replace(mstdir + binning + '\\Dark\\' + tagdate,\
+                                    outpath + usertag + '\\')
+            if not os.path.exists(outpath + usertag + '\\'):
+                os.mkdir(outpath + usertag + '\\')
+            try:
+                shutil.copy(fname, outname)
+            except:
+                shutil.copy(fname.replace('.fits', '_1.fits'), outname)
+
+
+if 'Flat' in types:
+    print 'Copying master flat files'
+    flat_files = tempfiles.where(tempfiles['type'] == 'Flat Field')
+    flat_files.dropna(how='all', inplace=True)
+    for band in filters:
+        flatband = flat_files.where(flat_files['filter'] == band)
+        flatband.dropna(how='all', inplace=True)
+        flatJD = flatband['JD']
+        for date in userJD:
+            year, month, day, sec = jd2gcal(sjd, (date - sjd))
+            usertag = '{}{}{}'.format(str(year), str(month).zfill(2), str(day).zfill(2))
+            flatdate = min(flatJD, key=lambda x: abs(x - date))
+            year, month, day, sec = jd2gcal(sjd, (flatdate - sjd))
+            tagdate = '{}{}{}'.format(str(year), str(month).zfill(2), str(day).zfill(2))
+            fname = '{}{}\\Flat\\{}\\master_flat_{}_{}_{}.fits'.format(mstdir, binning, tagdate, tagdate, binning, band)
+            outname = fname.replace(mstdir + binning + '\\Flat\\' + tagdate,\
                                     outpath + usertag + '\\')
             if not os.path.exists(outpath + usertag + '\\'):
                 os.mkdir(outpath + usertag + '\\')
